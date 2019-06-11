9b7988181943ab6d3932b1a9bc8ae63f86c60dc7
hexianghui <hxhsoft@foxmail.com>
2019-06-11 21:18:33 +0800

    从接口获取天气数据

---
+++ src/Weather.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: her-cat
+ * Date: 2019/6/11
+ * Time: 21:03
+ */
+
+namespace HerCat\Weather;
+
+
+use GuzzleHttp\Client;
+
+class Weather
+{
+    protected $key;
+    protected $guzzleOptions = [];
+
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
+    public function getHttpClient()
+    {
+        return new Client($this->guzzleOptions);
+    }
+
+    public function setGuzzleOptions(array $options)
+    {
+        $this->guzzleOptions = $options;
+    }
+
+    public function getWeather($city, string $type = 'base', string $format = 'json')
+    {
+        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+
+        $query = array_filter([
+            'key' => $this->key,
+            'city' => $city,
+            'extensions' => $type,
+            'output' => $format,
+        ]);
+
+        $response = $this->getHttpClient()->get($url, [
+            'query' => $query,
+        ])->getBody()->getContents();
+
+        return 'json' === $format ? \json_decode($response, true) : $response;
+    }
+}
