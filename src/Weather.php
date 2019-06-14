ac7fb4007e50b2f6cb9754f032694eddca090170
hexianghui <hxhsoft@foxmail.com>
2019-06-14 20:13:32 +0800

    兼容 PHP5.6 版本

--- src/Weather.php
+++ src/Weather.php
@@ -18,7 +18,7 @@ class Weather
     protected $key;
     protected $guzzleOptions = [];
 
-    public function __construct(string $key)
+    public function __construct($key)
     {
         $this->key = $key;
     }
@@ -43,7 +43,7 @@ class Weather
         return $this->getWeather($city, 'all', $format);
     }
 
-    public function getWeather($city, string $type = 'base', string $format = 'json')
+    public function getWeather($city, $type = 'base', $format = 'json')
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
