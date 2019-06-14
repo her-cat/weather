a67ea4944213ab2df5520cc749da504a24acd1f3
her-cat <hxhsoft@foxmail.com>
2019-06-14 12:29:41 +0000

    Apply fixes from StyleCI

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,13 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: her-cat
- * Date: 2019/6/11
- * Time: 22:33
+
+/*
+ * This file is part of the her-cat/weather.
+ *
+ * (c) her-cat <hxhsoft@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace HerCat\Weather;
-
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
